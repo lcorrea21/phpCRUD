2b5088186b5d15b072644148e5eba7a22be09ac4
lcorrea21 <44305869+lcorrea21@users.noreply.github.com>
2019-04-09 08:13:28 -0500

    Update connection.php

--- connection.php
+++ connection.php
@@ -13,7 +13,7 @@ $conn = mysql_connect("localhost","root","root")
  * using mysqli_connect instead
  */
 
-$databaseHost = '10.130.22.1';
+$databaseHost = 'localhost';
 $databaseName = 'test';
 $databaseUsername = 'root';
 $databasePassword = 'root';
